f0d5c58c6754b8f1fa196eb831fc8038a549ce87
Nicolas Vidal <nico.vidal@duocuc.cl>
2023-01-28 15:40:28 -0300

    first commit

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,24 @@
+import { Toolbar } from "@mui/material";
+import { Box } from "@mui/system"
+import { SideBar } from "../components";
+import { NavBar } from "../components/NavBar";
+
+const drawerWidth=240;
+
+export const JournalLayout = ({children}) => {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <NavBar drawerWidth={drawerWidth}/>
+            <SideBar drawerWidth={drawerWidth}/>
+            <Box
+                component='main'
+                sx={{ flexGrow: 1, p: 3 }}>
+
+                <Toolbar/>
+                {children}
+
+            </Box>
+
+        </Box>
+    )
+}
---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,14 @@
+
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+      <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet aliquam, iure consectetur aliquid dolorum officiis eum. Cumque facere omnis vel aperiam similique deleniti. Odit enim molestiae maiores accusamus dolorem harum?</Typography>
+    
+   
+    </JournalLayout>
+  )
+}
+``

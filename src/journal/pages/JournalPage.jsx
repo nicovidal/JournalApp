7de967112b5ab4ef03c4b502af2f248374733027
Nicolas Vidal <nico.vidal@duocuc.cl>
2023-01-29 17:09:22 -0300

    MockUp ok!

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,28 @@
 
-import { Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet aliquam, iure consectetur aliquid dolorum officiis eum. Cumque facere omnis vel aperiam similique deleniti. Odit enim molestiae maiores accusamus dolorem harum?</Typography>
-    
-   
+      
+       <NothingSelectedView/>
+      {/* <NoteView/> */}
+    <IconButton
+    size='large'
+    sx={{
+      color:'white',
+      backgroundColor:'error.main',
+      ':hover':{backgroundColor:'error.main',opacity:0.9},
+      position:'fixed',
+      right:50,
+      bottom:50
+    }}>
+      <AddOutlined sx={{fontSize:30}}/>
+
+    </IconButton>
     </JournalLayout>
   )
 }
